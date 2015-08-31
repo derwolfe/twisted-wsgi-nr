a42b12726b9d89a2b96d714ddc5d7300b9c703ad
derwolfe <chriswwolfe@gmail.com>
2015-08-30 20:47:19 -0500

    notes

--- twisted_wsgi_nr.py
+++ twisted_wsgi_nr.py
@@ -3,6 +3,7 @@
 import newrelic.agent
 newrelic.agent.initialize()
 
+import logging
 import falcon
 import json
 import os
@@ -13,8 +14,12 @@ import structlog
 from twisted.web.wsgi import WSGIResource
 from twisted.web.server import Site, Request
 from twisted.internet import reactor
-#from twisted.python.log import startLogging
-from twisted.logger import
+from twisted.python import log as twLog
+# from twisted.logger import (
+#     Logger, FileLogObserver, globalLogPublisher,
+#     jsonFileLogObserver
+# )
+
 
 logger = structlog.getLogger()
 
@@ -27,30 +32,34 @@ class QuoteResource(object):
             'quote': 'I\'ve always been more interested in the future than in the past.',
             'author': 'Grace Hopper'
         }
+        # this is a structlog logger
         log = logger.new(whom=quote["author"], what=quote["quote"])
         log.msg("quoted!")
         resp.body = json.dumps(quote)
 
 
 def run():
-    # api is the WSGI resource returned by Falcon.
+    # grab all of the events that are dispatched to stdlib logger
+    # new relic uses this.
+    handler = logging.StreamHandler(sys.stdout)
+    root_logger = logging.getLogger()
+    root_logger.addHandler(handler)
+
+    # start the twisted logger and begin capturing it.
+    twLog.startLogging(sys.stdout)
+
+    #globalLogPublisher.addObserver(jsonFileLogObserver)
     structlog.configure(
         processors=[
             structlog.processors.StackInfoRenderer(),
-            # either the json or the event can be used!
-            structlog.twisted.JSONRenderer(),
-            #structlog.twisted.EventAdapter()
+            structlog.twisted.JSONRenderer()
         ],
-        logger_factory=structlog.twisted.LoggerFactory(),
         context_class=dict,
+        logger_factory=structlog.twisted.LoggerFactory(),
         wrapper_class=structlog.twisted.BoundLogger,
         cache_logger_on_first_use=True,
     )
-    # configure a wrapper log observer
-
-
-    startLogging(sys.stdout)
-
+    # api is the WSGI resource returned by Falcon.
     api = falcon.API()
     api.add_route('/quote', QuoteResource())
 
